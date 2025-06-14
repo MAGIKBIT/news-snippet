eb3d6d9ec38b9ad61815eb847f49570ab4a9ecf8
MAGIKBIT <132863100+MAGIKBIT@users.noreply.github.com>
2025-06-14 05:53:00 +0000

    docker build flow fix

--- docker.sh
+++ docker.sh
@@ -3,5 +3,5 @@
 #cd newsplease
 #python __main__.py -c ./config
 cd /news-snippet/src/newsplease
+python3 -m pip install --upgrade pipconfig
 python3 __main__.py -c ./config
-pip install --upgrade pip
