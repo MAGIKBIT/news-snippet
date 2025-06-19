ae7fdc0ca6c5e4be454973b920dc0ca23d924e4b
MAGIKBIT <132863100+MAGIKBIT@users.noreply.github.com>
2025-06-19 03:44:26 +0000

    Refactor Dockerfile and docker.sh to adjust entrypoint and directory navigation for NewsExtraction

--- src/docker.sh
+++ src/docker.sh
@@ -7,7 +7,7 @@
 # python3 __main__.py -c ./config
 
 # Move to the NewsExtraction directory
-cd ./src/NewsExtraction
+# cd ./src/NewsExtraction
 
 # Run the package using the __main__.py entry point
 python3 __main__.py
